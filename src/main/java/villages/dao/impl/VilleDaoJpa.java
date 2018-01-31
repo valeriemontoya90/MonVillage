8b02a44133a6738c9a4f9fe41a707f23b9ce42e0
ValerieMontoya <valeriemontoya90@gmail.com>
2018-01-31 16:37:13 +0100

    fill VilleDaoJpa methods

--- src/main/java/villages/dao/impl/VilleDaoJpa.java
+++ src/main/java/villages/dao/impl/VilleDaoJpa.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Query;
 
+import application.Application;
 import villages.dao.VilleDao;
 import villages.model.Ville;
 
@@ -12,32 +14,139 @@ public class VilleDaoJpa implements VilleDao {
 
 	@Override
 	public Ville find(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+		Ville ville = null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			ville = em.find(Ville.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null) {
+				tx.rollback();
+			}
+		} finally {
+
+			if (em != null) {
+				em.close();
+			}
+		}
+		return ville;
 	}
 
 	@Override
 	public List<Ville> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Ville> liste = null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			Query query = em.createQuery("from Ville");
+			liste = query.getResultList();
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null) {
+				tx.rollback();
+			}
+		} finally {
+
+			if (em != null) {
+				em.close();
+			}
+		}
+		return liste;
 	}
 
 	@Override
 	public void create(Ville obj) {
-		// TODO Auto-generated method stub
-		
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+			
+			em.persist(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null) {
+				tx.rollback();
+			}
+		} finally {
+
+			if (em != null) {
+				em.close();
+			}
+		}
 	}
 
 	@Override
 	public Ville update(Ville obj) {
-		// TODO Auto-generated method stub
-		return null;
+		Ville objCopy = null;
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			objCopy = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null) {
+				tx.rollback();
+			}
+		} finally {
+
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return objCopy;
 	}
 
 	@Override
 	public void delete(Ville obj) {
-		// TODO Auto-generated method stub
-		
-	}
+		EntityManager em = null;
+		EntityTransaction tx = null;
 
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			em.remove(em.merge(obj));
+
+			tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (tx != null) {
+				tx.rollback();
+			}
+		} finally {
+
+			if (em != null) {
+				em.close();
+			}
+		}
+	}
 }
