4bf09966e221401340788da314952f537e3075b9
asterda <asterda@gmail.com>
2018-01-31 16:16:07 +0100

    add dao pays

---
+++ src/main/java/villages/model/Pays.java
@@ -0,0 +1,40 @@
+package villages.model;
+
+public class Pays {
+
+	public int id;
+	public String code;
+	public String libelle;
+
+	public Pays(int id, String code, String libelle) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.libelle = libelle;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getLibelle() {
+		return libelle;
+	}
+
+	public void setLibelle(String libelle) {
+		this.libelle = libelle;
+	}
+
+}
